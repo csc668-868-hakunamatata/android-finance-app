7a00253f6593e38d648253ecf898a1ff41463302
white <white.freddie95@gmail.com>
2021-04-01 19:26:09 -0700

    working camera. Uses imageView to see it.

--- app/src/main/java/com/example/financeapp/MyCameraActivity.java
+++ app/src/main/java/com/example/financeapp/MyCameraActivity.java
@@ -5,8 +5,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +23,14 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
 
 public class MyCameraActivity extends Activity
 {
@@ -68,11 +85,52 @@ public class MyCameraActivity extends Activity
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        int rotationDegree = 0;
 
         if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK)
         {
+
+            //Photo right here
             Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+            imageView.setImageBitmap(photo); //displays photo as bitmap into imageView
+
+            InputImage image = InputImage.fromBitmap(photo, rotationDegree);
+            TextRecognizer recognizer = TextRecognition.getClient();
+
+            //Process text captured from image
+            Task<Text> result = recognizer.process(image)
+                    .addOnSuccessListener(new OnSuccessListener<Text>() {
+                        @Override
+                        public void onSuccess(Text text) {
+                            String resultText = text.getText();
+                            System.out.println("Here");
+                            System.out.println(resultText);
+                            for(Text.TextBlock block : text.getTextBlocks()) {
+                                String blockText = block.getText();
+                                Point[] blockCornerPoints = block.getCornerPoints();
+                                Rect blockFrame = block.getBoundingBox();
+                                for (Text.Line line : block.getLines()) {
+                                    String lineText = line.getText();
+                                    Point[] lineCornerPoints = line.getCornerPoints();
+                                    Rect lineFrame = line.getBoundingBox();
+                                    System.out.println("Line: " + line.toString());
+                                    for (Text.Element element : line.getElements()) {
+                                        String elementText = element.getText();
+                                        Point[] elementCornerPoints = element.getCornerPoints();
+                                        Rect elementFrame = element.getBoundingBox();
+                                    }
+                                }
+                                System.out.println("Block text: " + blockText);
+                            }
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
         }
     }
+
 }
